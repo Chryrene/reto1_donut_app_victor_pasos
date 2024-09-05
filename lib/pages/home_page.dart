f41acf567b25c241ed60682b31b953a032ea3bca
Chryrene <victorpasos2001@gmail.com>
2024-09-05 15:18:27 -0600

    Añadiendo texto principal

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,8 +12,54 @@ class _HomepageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        leading: Icon(Icons.menu, color: Colors.grey[800]),
+        backgroundColor: Colors.transparent,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 24.0),
+          child: IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Colors.grey[800],
+              size: 36,
+            ),
+            onPressed: () {
+              print('Te amo Ulises');
+            },
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: IconButton(
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.grey[800],
+                  size: 36,
+                ),
+                onPressed: () {
+                  print('Te amo');
+                }),
+          )
+        ],
+      ),
+      body: const Column(
+        children: [
+          //Texto "I want to eat"
+          Padding(
+            padding: EdgeInsets.all(32.0),
+            child: Row(
+              children: [
+                Text(
+                  'I want to ',
+                  style: TextStyle(fontSize: 32),
+                ),
+                Text(
+                  'eat',
+                  style: TextStyle(fontSize: 32),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
